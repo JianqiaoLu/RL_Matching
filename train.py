7119fa7135234d59ec5d5c84f70957f18fcb41e2
lujianqiao <tjlujianqiao@gmail.com>
2023-04-21 18:39:06 +0800

    onlinebipartitematching

---
+++ train.py
@@ -0,0 +1,73 @@
+import gymnasium as gym
+import torch.optim as optim
+from itertools import count
+from memory import ReplayMemory
+from trainer import *
+from utils import TrainConfig, RL_TrainConfig
+from environment import BinPackingActionMaskGymEnvironment, BipartiteMatchingActionMaskGymEnvironment
+
+# set up matplotlib
+plt.ion()
+
+# if gpu is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+DQN_config = TrainConfig(
+    BATCH_SIZE=128,
+    GAMMA=0.99,
+    EPS_START=0.9,
+    EPS_END=0.05,
+    EPS_DECAY=1000,
+    TAU=0.005,
+    LR=1e-4,
+)
+
+RL_config = RL_TrainConfig(
+    BATCH_SIZE=128,
+    SAVE_PATH='/Users/jianqiaolu/discuss with zhiyi/rl/RL_model',
+    SAVE_INTERVAL=1,
+    LR=1e-4,
+)
+env_config = {
+    "bag_capacity": 100,
+    'item_sizes': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'item_probabilities': [0, 0, 0, 1 / 3, 0, 0, 0, 0, 2 / 3],  # linear waste
+    # 'item_probabilities': [0.14, 0.10, 0.06, 0.13, 0.11, 0.13, 0.03, 0.11, 0.19], #bounded waste
+    # 'item_probabilities': [0.06, 0.11, 0.11, 0.22, 0, 0.11, 0.06, 0, 0.33], #perfect pack
+    'time_horizon': 1000
+}
+
+if __name__ == '__main__':
+    # set env
+    # env = gym.make("CartPole-v1")
+    env = BinPackingActionMaskGymEnvironment()
+    # env = BipartiteMatchingActionMaskGymEnvironment()
+    # env.read_graph_from_file("real_graph/lp_blend/lp_blend.mtx")
+    # Get number of actions from gym action space
+    n_actions = env.action_space.n
+    # Get the number of state observations
+    state = env.reset()
+    # state = env.reset()
+
+    # DQN_trainer model
+
+    # make experience for getting reward
+    n_observations = len(state['real_obs'])
+    # n_observations = len(state)
+
+    policy_net = DQN(n_observations, n_actions).to(device)
+    target_net = DQN(n_observations, n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
+
+    # only set optimizer for policy_net
+    optimizer = optim.AdamW(policy_net.parameters(), lr=DQN_config.LR, amsgrad=True)
+    memory = ReplayMemory(10000)
+    dqn_trainer = DQN_Trainer(policy_net, target_net, memory, optimizer, env, DQN_config, device)
+    dqn_trainer.train()
+
+    # REINFORCE trainer model
+    policy_net2 = policy_estimator(n_observations, n_actions)
+    optimizer = optim.AdamW(policy_net2.parameters(), lr=RL_config.LR, amsgrad=True)
+
+    rl_trainer = REINFORCE_trainer(policy_net2, optimizer, env, RL_config, device)
+    # rl_trainer.train()
---
+++ utils.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import Dict, Any, Optional
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+@dataclass
+class TrainConfig:
+    # BATCH_SIZE is the number of transitions sampled from the replay buffer
+    # GAMMA is the discount factor as mentioned in the previous section
+    # EPS_START is the starting value of epsilon
+    # EPS_END is the final value of epsilon
+    # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    # TAU is the update rate of the target network
+    # LR is the learning rate of the AdamW optimizer
+    BATCH_SIZE : int
+    GAMMA : float
+    EPS_START : float
+    EPS_END : float
+    EPS_DECAY : int
+    TAU : float
+    LR : float
+    @classmethod
+    def from_dict(cls, config: Dict[str, Any]):
+        return cls(**config)
+
+
+
+@dataclass
+class RL_TrainConfig:
+    # BATCH_SIZE is the number of transitions sampled from the replay buffer
+    # GAMMA is the discount factor as mentioned in the previous section
+    # EPS_START is the starting value of epsilon
+    # EPS_END is the final value of epsilon
+    # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    # TAU is the update rate of the target network
+    # LR is the learning rate of the AdamW optimizer
+    BATCH_SIZE : int
+    SAVE_PATH: str
+    SAVE_INTERVAL: int
+    LR : float
+    @classmethod
+    def from_dict(cls, config: Dict[str, Any]):
+        return cls(**config)
